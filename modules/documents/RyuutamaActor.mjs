422f7a56dfde94a57f1cb65a27785d3b4c05b94b
Petwag <fhortet.petwag@gmail.com>
2023-09-06 01:00:24 +0200

    Add correct condition roll and start competence selector

--- modules/documents/RyuutamaActor.mjs
+++ modules/documents/RyuutamaActor.mjs
@@ -1,19 +1,42 @@
 export class RyuutamaActor extends Actor {
   /** @override */
   prepareData() {
+    this.selectedStat = [];
+
     super.prepareData();
   }
 
   rollCondition() {
-    const newCondition =
-      parseInt(this.system.stats.str.die) + parseInt(this.system.stats.spi.die);
+    const strDice = this.system.stats.str.die;
+    const spiDice = this.system.stats.spi.die;
 
-    this.update({ "system.condition": newCondition });
+    const roll = new Roll(`1d${strDice} + 1d${spiDice}`);
 
-    return ChatMessage.create({
-      user: game.user._id,
-      speaker: ChatMessage.getSpeaker({ actor: game.user._id }),
-      content: `Rolling Condition for ${this.name}... [STR + SPI]`,
+    roll.evaluate().then(() => {
+      this.update({ "system.condition": roll.total });
+
+      return ChatMessage.create({
+        user: game.user._id,
+        speaker: ChatMessage.getSpeaker({ actor: game.user._id }),
+        content: `Rolling Condition for ${this.name}... [STR + SPI]`,
+      });
     });
+  }
+
+  selectStat(stat) {
+    console.log(stat, this.selectedStat);
+    if (this.selectedStat.includes(stat)) {
+      this.selectedStat = this.selectedStat.filter((s) => s !== stat);
+    } else {
+      if (this.selectedStat.length >= 2) {
+        this.selectedStat.shift();
+      }
+
+      this.selectedStat.push(stat);
+    }
+
+    console.log(this.selectedStat);
+
+    this.update({ selectedStat: this.selectedStat });
   }
 }
--- modules/sheets/RyuutamaActorSheet.mjs
+++ modules/sheets/RyuutamaActorSheet.mjs
@@ -26,12 +26,13 @@ export class RyuutamaActorSheet extends ActorSheet {
 
     context.config = CONFIG.ryuutama;
     context.system = itemData.system;
-    context.spells = this._getSpells(itemData.items);
-    context.skills = this._getSkills(itemData.items);
-    context.maxHp = this._getMaxHp(itemData.system);
-    context.maxMp = this._getMaxMp(itemData.system);
+    context.spells = this.getSpells(itemData.items);
+    context.skills = this.getSkills(itemData.items);
+    context.maxHp = this.getMaxHp(itemData.system);
+    context.maxMp = this.getMaxMp(itemData.system);
+    context.selectedStat = itemData.selectedStat;
 
-    console.log(context.system);
+    console.log(itemData.selectedStat);
     return context;
   }
 
@@ -39,29 +40,40 @@ export class RyuutamaActorSheet extends ActorSheet {
     super.activateListeners(html);
 
     if (this.actor.isOwner) {
-      html.find(".condition-roll").click(this._onConditionRoll.bind(this));
+      html.find(".condition-roll").click(this.onConditionRoll.bind(this));
+      html.find(".stat-item").click(this.onStatSelect.bind(this));
     }
   }
 
   // Data Getter
-  _getSpells(items) {
+  getSpells(items) {
     return items.filter((item) => item.type === "spell");
   }
 
-  _getSkills(items) {
+  getSkills(items) {
     return items.filter((item) => item.type === "skill");
   }
 
-  _getMaxHp(system) {
+  getMaxHp(system) {
     return system.stats.str.die * 2;
   }
 
-  _getMaxMp(system) {
+  getMaxMp(system) {
     return system.stats.spi.die * 2;
   }
 
   // Event Handlers
-  _onConditionRoll() {
+  onConditionRoll() {
     this.actor.rollCondition();
+  }
+
+  onStatSelect(event) {
+    const dataStat = event.currentTarget.attributes["data-stat"];
+
+    if (!dataStat || !dataStat.value) return;
+
+    const stat = dataStat.value;
+
+    this.actor.selectStat(stat);
   }
 }
