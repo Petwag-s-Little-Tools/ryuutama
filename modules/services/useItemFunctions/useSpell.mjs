6ab75ce86c7f0f80fb2ceae8086a28db1c21726e
Petwag <fhortet.petwag@gmail.com>
2023-12-02 00:53:39 +0100

    Revert "delete displayInChat"
    This reverts commit b04dae54ea4b3c803236f90d50965f0b55e0e62f.

--- modules/services/useItemFunctions/useSpell.mjs
+++ modules/services/useItemFunctions/useSpell.mjs
@@ -1,4 +1,5 @@
 import { RyuutamaItem } from "../../documents/RyuutamaItem.mjs";
+import { displayInChat } from "./displayInChat.mjs";
 
 /**
  *
@@ -7,45 +8,4 @@ import { RyuutamaItem } from "../../documents/RyuutamaItem.mjs";
  */
 export const useSpell = async (item) => {
   return await displayInChat(item);
-};
-
-/**
- * Display the item card
- * @param {RyuutamaItem} item
- * @param {{}} [setup={}]
- * @returns {ChatMessage}
- */
-export const displayInChat = async (item, setup = {}) => {
-  const token = item.actor.token;
-
-  const templateData = {
-    actor: item.actor,
-    item: item,
-    tokenId: token?.uuid || null,
-    system: item.system,
-    setup,
-  };
-
-  const html = await renderTemplate(
-    "systems/ryuutama/templates/chat/item-card.hbs",
-    templateData
-  );
-
-  // create chat message
-  const chatData = {
-    user: game.user.id,
-    type: CONST.CHAT_MESSAGE_TYPES.OTHER,
-    content: html,
-    flavor: item.system.flavor,
-    speaker: ChatMessage.getSpeaker({ actor: item.actor, token }),
-    flags: { "core.canPopout": true },
-  };
-
-  // Apply correct visibility
-  ChatMessage.applyRollMode(chatData, game.settings.get("core", "rollMode"));
-
-  // Create the card
-  const card = await ChatMessage.create(chatData);
-
-  return card;
 };
