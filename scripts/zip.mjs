90b3b2e07fdd6308e91c2894e93e02634620828b
Petwag <fhortet.petwag@gmail.com>
2024-03-03 22:54:17 +0100

    fix zip

--- scripts/zip.mjs
+++ scripts/zip.mjs
@@ -2,31 +2,35 @@ import { compilePack } from "@foundryvtt/foundryvtt-cli";
 import AdmZip from "adm-zip";
 import p from "../package.json" assert { type: "json" };
 
-const zip = new AdmZip();
+const main = async () => {
+  const zip = new AdmZip();
 
-const files = [
-  "./ryuutama.mjs",
-  "./ryuutama.css",
-  "./ryuutama.css.map",
-  "system.json",
-  "template.json",
-];
+  const files = [
+    "./ryuutama.mjs",
+    "./ryuutama.css",
+    "./ryuutama.css.map",
+    "system.json",
+    "template.json",
+  ];
 
-files.forEach((file) => {
-  zip.addLocalFile(file);
-});
+  files.forEach((file) => {
+    zip.addLocalFile(file);
+  });
 
-const folders = ["./lang", "./static", "./templates"];
+  const folders = ["./lang", "./static", "./templates"];
 
-folders.forEach((folder) => {
-  zip.addLocalFolder(folder, folder);
-});
+  folders.forEach((folder) => {
+    zip.addLocalFolder(folder, folder);
+  });
 
-const packs = ["skills", "spells"];
+  const packs = ["skills", "spells"];
 
-packs.forEach((pack) => {
-  compilePack(`./packs/${pack}`, `./tmpPacks/${pack}`);
-});
-zip.addLocalFolder("./tmpPacks", "./packs");
+  for await (const pack of packs) {
+    await compilePack(`./packs/${pack}/_source`, `./tmpPacks/${pack}`);
+  }
+  zip.addLocalFolder("./tmpPacks", "./packs");
 
-zip.writeZip(`./ryuutama-release-${p.version}.zip`);
+  zip.writeZip(`./ryuutama-release-${p.version}.zip`);
+};
+
+main();
